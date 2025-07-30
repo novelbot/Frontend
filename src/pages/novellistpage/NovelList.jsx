af55022250cfba040c432c02bc883912cb4c60fb
sunoojin <sunoojin17@gmail.com>
2025-07-30 12:01:36 +0900

    폰트 크기 조절,  키보드 이벤트 추가, NovelCard.jsx 생성

--- src/pages/novellistpage/NovelList.jsx
+++ src/pages/novellistpage/NovelList.jsx
@@ -1,34 +1,22 @@
 // src/mainpage/NovelList.jsx
 import React from "react";
-import { Link } from "react-router-dom";
 import "./NovelList.css";
 import { novels } from "/src/data/novelData";
+import NovelCard from "./NovelCard";
 
 const NovelList = () => {
-  const toSlug = (title) => title.replace(/\s+/g, ""); // 공백 제거
-
   return (
     <div className="novel-list-container">
       <h3>웹소설</h3>
       <div className="novel-grid">
         {novels.map((novel, index) => (
-          <Link
+          <NovelCard
             key={index}
-            to={`/MainPage/${toSlug(novel.title)}`}
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            <div className="novel-card" style={{ cursor: "pointer" }}>
-              <img
-                src={novel.coverImageUrl}  // 또는 novel.image 사용 중이면 맞게 조정
-                alt={novel.title}
-                className="novel-image"
-              />
-              <div className="novel-overlay">
-                <h4>{novel.title}</h4>
-                <p>{novel.genre} · {novel.author}</p>
-              </div>
-            </div>
-          </Link>
+            title={novel.title}
+            genre={novel.genre}
+            author={novel.author}
+            imageUrl={novel.coverImageUrl}
+          />
         ))}
       </div>
     </div>

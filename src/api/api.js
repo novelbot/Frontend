29e50d64743763c7305cd35b7016ae49713bff28
JaeHun <ryu040328@naver.com>
2025-08-05 13:00:41 +0900

    api 연결

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const instance = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+
+    withCredentials: false,
+    headers:{
+        "Content-Type": "application/json",
+    },
+    timeout: 10000,
+});
--- src/pages/novellistpage/NovelList.jsx
+++ src/pages/novellistpage/NovelList.jsx
@@ -1,10 +1,29 @@
 // src/mainpage/NovelList.jsx
 import React from "react";
+import { useEffect } from "react";
 import "./NovelList.css";
 import { novels } from "/src/data/novelData";
 import NovelCard from "./NovelCard";
+import { instance } from "../../api/api";
 
 const NovelList = () => {
+
+  useEffect(() => {
+    const getNovelList = async () => {
+      try {
+        const res = await instance.get('/novels/1/episodes');
+        const data = res
+        console.log(data);
+        return res;
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+
+    getNovelList();
+  }, [])
+
   return (
     <div className="novel-list-container">
       <h3>웹소설</h3>

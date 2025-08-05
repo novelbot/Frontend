1ab667937fdd95eed672e000df796876580b4d07
sunoojin <sunoojin17@gmail.com>
2025-08-05 13:07:42 +0900

    NovelList API 받아오기

--- src/pages/novellistpage/NovelList.jsx
+++ src/pages/novellistpage/NovelList.jsx
@@ -1,36 +1,34 @@
 // src/mainpage/NovelList.jsx
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./NovelList.css";
-import { novels } from "/src/data/novelData";
 import NovelCard from "./NovelCard";
-import { instance } from "../../api/api";
+import { instance } from "../../API/api";
 
 const NovelList = () => {
+  const [novelList, setNovelList] = useState([]);
 
   useEffect(() => {
     const getNovelList = async () => {
       try {
-        const res = await instance.get('/novels/1/episodes');
-        const data = res
-        console.log(data);
-        return res;
+        const res = await instance.get("/novels");
+        const data = res.data;
+        setNovelList(data); // 상태에 저장
       } catch (err) {
-        console.log(err);
-        throw err;
+        console.error("소설 목록 불러오기 실패:", err);
       }
-    }
+    };
 
     getNovelList();
-  }, [])
+  }, []);
 
   return (
     <div className="novel-list-container">
       <h3>웹소설</h3>
       <div className="novel-grid">
-        {novels.map((novel, index) => (
+        {novelList.map((novel) => (
           <NovelCard
-            key={index}
+            key={novel.novelId}
             title={novel.title}
             genre={novel.genre}
             author={novel.author}
@@ -43,3 +41,46 @@ const NovelList = () => {
 };
 
 export default NovelList;
+
+// const NovelList = () => {
+//   const [novelList, setNovelList] = useState([]);
+//   const navigate = useNavigate();
+
+//   const handleCardClick = (id) => {
+//     navigate(`/MainPage/${id}`);
+//   };
+
+//   useEffect(() => {
+//     const getNovelList = async () => {
+//       try {
+//         const res = await instance.get("/novels");
+//         setNovelList(res.data);
+//       } catch (err) {
+//         console.error("소설 목록 불러오기 실패:", err);
+//       }
+//     };
+
+//     getNovelList();
+//   }, []);
+
+//   return (
+//     <div className="novel-list-container">
+//       <h3>웹소설</h3>
+//       <div className="novel-grid">
+//         {novelList.map((novel) => (
+//           <div
+//             key={novel.novelId}
+//             onClick={() => handleCardClick(novel.novelId)}
+//           >
+//             <NovelCard
+//               title={novel.title}
+//               genre={novel.genre}
+//               author={novel.author}
+//               imageUrl={novel.coverImageUrl}
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
